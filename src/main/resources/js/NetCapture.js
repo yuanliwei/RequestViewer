e0a0be7fee68d61b61f5761a2d5fabe11944cc27
yuanliwei <891789592@qq.com>
2017-04-07 02:24:37 +0800

    add fast list
    Signed-off-by: yuanliwei <891789592@qq.com>

--- src/main/resources/js/NetCapture.js
+++ src/main/resources/js/NetCapture.js
@@ -41,3 +41,40 @@ function testStopCapture() {
 	NetCapture.stop();
 	console.log('stop capture.');
 }
+
+$(document).ready(function () {
+	var datas = [];
+	for (var i = 0; i < 3000; i++) {
+		datas.push({'wordNo':i, 'desc': 'descdescdesc', 'wrongNum': 23});
+	}
+	var list = null;
+	setTimeout(function () {
+		var root = $("#root");
+		list = new FastList(root);
+		list.setHandle({
+			createViewHolder: function (view) {
+				var holder = {
+					wordNo: $(view.find('.wordNo')),
+					desc: $(view.find('.desc')),
+					wrongNum: $(view.find('.wrongNum')),
+					descBg: $(view.find('.descBg')),
+					item: $(view.find('.row'))
+				}
+				return holder;
+			},
+			bindData: function (position, holder, data) {
+				holder.wordNo.text(data.wordNo);
+				holder.desc.text(data.desc);
+				holder.wrongNum.text(data.wrongNum);
+				holder.item.removeClass("bg-info bg-primary");
+				if (position%2 == 0) {
+					holder.item.addClass("bg-info");
+				} else {
+					holder.item.addClass("bg-primary");
+				}
+			}
+		});
+		list.setDatas(datas);
+		list.log();
+	},1000);
+});
